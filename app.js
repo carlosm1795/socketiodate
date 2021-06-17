8c2b0d3be229d92c81520ee6c25a9063586ea7e0
Carlos <carlosm1795@gmail.com>
2021-06-16 18:24:50 -0600

    Adding Cors

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const index = require("./routes/index");
 
 const app = express();
 app.use(index);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 const server = http.createServer(app);
 
